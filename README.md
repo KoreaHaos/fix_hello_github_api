094ce7c4a66dfb4ffb208b8aff6677a0a22874c7
KoreaHaos <benjaminhaos@ewha.ac.kr>
2016-02-13 10:47:50 +0000

    Some fixes

--- README.md
+++ README.md
@@ -1,7 +1,2 @@
 # hello_github_api
 
-in progress...
-
-Haiving problems commiting,
-
-maybe you can't nest repos?
--- js/display_object_w_jsc3d.js
+++ js/display_object_w_jsc3d.js
@@ -16,60 +16,32 @@ var displayedFinalWords = false;
 // As it says, higher numbers rotate the object slower.
 var rotationLag = 10;
 
-show_viewer(xRotInitial, yRotInitial, zRotInitial);
-
-/*
-function loadImage(img_to_load){
-    
-    var img = new Image();
-    
-    $(img).load(function(){
-    
-      $('.the_top_left_corner').append($(this));
-    
-    }).attr({
-    
-      src: '../img/GitCatsMashUp.png'
-    
-    }).error(function(){
-      //do something if image cannot load
-    });
-}
-*/
 
 function displayPartOneOfMessageToUser() {
     console.log("First Bit!");
-    var imgPath = "../img/click.gif";
+    var imgPath = "../img/clickTrans.gif";
     loadImage(imgPath);
-    
-    
     displayedFirstWord = true;
 }
 
 function displayPartTwoOfMessageToUser() {
     console.log("Second Bit!");
-    var imgPath = "../img/asYouPlease.gif";
+    var imgPath = "../img/as_you_pleaseTrans.gif";
     loadImage(imgPath);
     displayedFinalWords = true;
 
 }
 
 function loadImage(path_to_image_to_place_on_dom) {
-    
     var img = new Image();
-    
     $(img).load(function(){
-    
       $('.overlayed_message').append($(this));
-        console.log("First Bit! ajaxed" + this);
+        //console.log("First Bit! ajaxed" + this);
     }).attr({
-    
       src: path_to_image_to_place_on_dom
-    
     }).error(function(){
       //do something if image cannot load
     });    
-    
 }
 
 
@@ -77,14 +49,10 @@ function show_viewer(xRotIn, yRotIn, zRotIn) {
 
     // Get the canvas from The Don
     var cvs = document.getElementById('object_canvas');
-
-
     cvs.width = window.innerWidth;
     cvs.height = window.innerHeight;
 
     var viewer = new JSC3D.Viewer(cvs);
-
-    viewer.setParameter('SceneUrl', '../obj/git_hub_logo.obj');
 
     viewer.setParameter('InitRotationX', xRotIn);
     viewer.setParameter('InitRotationY', yRotIn);
@@ -92,33 +60,40 @@ function show_viewer(xRotIn, yRotIn, zRotIn) {
 
     viewer.setParameter('ModelColor', '#1a1a1a');
 
-    viewer.setBackgroudImageFromUrl('../img/GitCatsMashUp.png');
-
     viewer.setParameter('RenderMode', 'texturesmooth');
 
+    viewer.setBackgroudImageFromUrl('../img/GitCatsMashUp.png');
+    viewer.setParameter('SceneUrl', '../obj/git_hub_logo.obj');
+    
     viewer.init();
 
     // This bit takes care of the aniumation.
-
     setInterval(function() {
         viewer.rotate(xRotAdjust, yRotAdjust, zRotAdjust);
         viewer.update();
+        
+        // This code displays a two part message to the client.
+        
         // ToDo put this into a switch...
         if (!displayedFinalWords) {
             rotationCount += 1;
         }
+        
+        // If the object has spun one 360 degree rotation...
         if (rotationCount > 360 && !displayedFirstWord) {
             displayPartOneOfMessageToUser();
         }
+        // If the object has spun two 360 degree rotations (But not anymore)...
         if (rotationCount > 720 && displayedFirstWord && !displayedFinalWords) {
             displayPartTwoOfMessageToUser();
         }
         else if (rotationCount % 45 == 0) {
-            console.log("degrees rotated = " + rotationCount);
+            // Uncommenting this will display objects degree.
+            //console.log("degrees rotated = " + rotationCount);
         }
     }, rotationLag);
 }
 
-// That's it!
+show_viewer(xRotInitial, yRotInitial, zRotInitial);
 
-// Source : i'm sure i script-kiddied this at some level, but i lost the source, google 'JSC3D' and the method you are interested in and you'll find what you need.
+// That's it!
